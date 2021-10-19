65660e03ff33ec71fa7f5f75ce891cb0b1aea452
StoRmy1142 <78028961+StoRmy1142@users.noreply.github.com>
2021-10-19 19:18:27 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,25 @@
+Config = {}
+
+Config.Debug = false
+
+Config.Webhook = ''
+Config.ServerName = 'Bigamer'
+
+Config.MessageId = ''
+
+Config.UpdateTime = 1000*60*1 
+Config.Use24hClock = true 
+Config.JoinLink = 'fivem://connect/185.185.134.21' 
+
+Config.EmbedColor = 3158326
+
+Config.Locale = 'en'
+
+Config.Locales = {
+    ['en'] = {
+        ['date'] = 'Date',
+        ['time'] = 'Time',
+        ['players'] = 'Players',
+        ['connect'] = 'Connect to server',
+    }
+}
